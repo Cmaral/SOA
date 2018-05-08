276b73e9885298e6d110413dc8238043b3c399d8
Magnusson19 <36761142+Magnusson19@users.noreply.github.com>
2018-05-08 15:36:10 +0200

    Add files via upload

--- user.c
+++ user.c
@@ -3,10 +3,14 @@
 
 char buff[24];
 
+int sys_get_stats(int pid, struct stats *st);
+void exit();
+
 //Mostra per pantalla els stats del process amb pid task_pid
 void stats(int task_pid) {
 	int total_time;
 	struct stats task_stats;
+	get_stats(task_pid, &task_stats);
 
 
 	itoa(task_pid,buff);
@@ -38,35 +42,80 @@ void stats(int task_pid) {
 
 // All processes CPU bounded
 void workload1()  {
-	int i;
-
-	for (i=0; i<2; ++i) {
-
+	int pid1 = 0;
+	int n = 2;
+	int m = 2;
+	pid1 = fork();
+	int pid2 = 0;
+	if (pid1 == 0) {
+			for (int i = 0; i < 1000000; i++) {
+			if (n > 0) n = n*n;
+		}
+		stats(getpid());
+		exit();
 	}
-
-
+	pid2 = fork();
+	if (pid2 == 0) {
+		for (int i = 0; i < 1000000; i++) {
+			if (m > 0) m = m*m;
+		}
+		stats(getpid());
+		exit();
+	}
+	exit();
 }
 
 // All processes I/O Bounded
 void workload2()  {
-	int i;
-
-	for (i=0; i<2; ++i) {
-		fork();
+	int pid1, pid2;
+	pid1 = fork();
+	if (pid1 == 0) {
+		read(0,0,1024);
+		stats(getpid());
+		exit();
 	}
-
-	read(0,0,1024);
-	stats(getpid());
-	stats(0); // també del idle
+	pid2 = fork();
+	if (pid2 == 0) {
+		read(0,2048,1024);
+		stats(getpid());
+		exit();
+	}
+	exit();
 }
 
 // I/O bounded and CPU bounded
 void workload3()  {
-	int i;
-
-	for (i=0; i<2; ++i) {
-		
+	int pid1 = 0;
+	int n = 2;
+	int m = 2;
+	pid1 = fork();
+	int pid2 = 0;
+	if (pid1 == 0) {
+		for (int i = 0; i < 1000000; i++) {
+			if (n > 0) n = n*n;
+		}
+		read(0,0,1024);
+		n = 2;
+		for (int i = 0; i < 1000000; i++) {
+			if (n > 0) n = n*n;
+		}
+		stats(getpid());
+		exit();
 	}
+	pid2 = fork();
+	if (pid2 == 0) {
+		for (int i = 0; i < 1000000; i++) {
+			if (m > 0) m = m*m;
+		}
+		read(0,2048,1024);
+		m = 2;
+		for (int i = 0; i < 1000000; i++) {
+			if (m > 0) m = m*m;
+		}
+		stats(getpid());
+		exit();
+	}
+	exit();
 }
 
 // I/O bursts and CPU bursts
@@ -87,6 +136,9 @@ int __attribute__ ((__section__(".text.main")))
 
     
    //Hauriem de tenir una manera de canviar entre RR i FCFS
-	
+	set_sched_policy(0);
+	//workload1();
+	//workload2();
+	//workload3();
   while(1) { }
 }
